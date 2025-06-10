5e8149b1fe6b802acc403bc3a8a5c525ef55aaa1
Djane Soares <djanesoares@gmail.com>
2025-06-10 15:58:29 +0100

    Spring Security funcionamento

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEncoder.encode( "123456"));
+
+		// COMO E FEITA A COMPARAÇAO - verificaçao de senha
+
+		boolean result = passwordEncoder.matches("123456", "$2a$10$fKy2OkNcCq9jgXqCAbAbi.Ws3ljRU2qebZ4XWzU1Uig4jVTFSPYdy");
+		System.out.println("Resultado = " + result);
+	}
+
 
 }
