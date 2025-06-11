0ed2254cc11580e5bf0e71d625d6277e97f4c515
Djane Soares <djanesoares@gmail.com>
2025-06-11 08:11:52 +0100

    BCrypt password

--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -15,13 +15,13 @@ public class SecurityConfig {
        return new BCryptPasswordEncoder();
    }
 
-
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable());
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
+
+
 
 }
