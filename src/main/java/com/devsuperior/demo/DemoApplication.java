77f986c6dac382eb72ebc4d5056c459817c2a41d
Djane Soares <djanesoares@gmail.com>
2025-07-02 07:59:06 +0100

    acess control by route, by role

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,10 +1,16 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication  {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 
 	public static void main(String[] args) {
@@ -12,4 +18,11 @@ public class DemoApplication  {
 	}
 
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+
+		boolean result = passwordEncoder.matches("123456", "$2a$10$sdy/PfaUp43.49lL85d3z.I2h.NXcQbngLtECXV2bGlRFDyyhPpSq");
+		System.out.println("Resultado = " + result);
+	}
 }
